2f4bb183d58779e963e3ec7d1517191f2feba4b9
GproSoftware828 <JetSendProductions@Gmail.com>
2018-05-28 08:48:42 -0500

    Installed redux form framework

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Dashboard = () => {
+	return (
+		<div>
+			Dashboard
+			<a class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">add</i></a>
+		</div>
+	);
+};
+
+export default Dashboard;
